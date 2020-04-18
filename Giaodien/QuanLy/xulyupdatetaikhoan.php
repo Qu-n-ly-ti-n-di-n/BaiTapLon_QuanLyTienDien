7da1974d30becb75485be93f85e619370a871d8a
= <=>
2020-04-18 14:16:04 +0700

    signin

--- Giaodien/QuanLy/xulyupdatetaikhoan.php
+++ Giaodien/QuanLy/xulyupdatetaikhoan.php
@@ -1,8 +1,9 @@
 <?php 
     include '../../FormDangNhap/connect.php';
+    $hashpass=password_hash($_POST['mkhau'],PASSWORD_DEFAULT);
     if(isset($_POST['userID']) && isset($_POST['mkhau']) && isset($_POST['name']))
     {
-        $update = "UPDATE user SET userID = '".$_POST['userID']."', passWord = '".$_POST['mkhau']."', name ='".$_POST['name']."' where userID ='". $_GET['id']."'";     
+        $update = "UPDATE user SET userID = '".$_POST['userID']."', passWord = '".$hashpass."', name ='".$_POST['name']."' where userID ='". $_GET['id']."'";     
         if(empty($_POST['userID']) || empty($_POST['mkhau']) || empty($_POST['name']))
         {
             echo "Mời bạn nhập đầy đủ thông tin .<a href='javascript: history.go(-1)'>Trở lại</a>"; 
